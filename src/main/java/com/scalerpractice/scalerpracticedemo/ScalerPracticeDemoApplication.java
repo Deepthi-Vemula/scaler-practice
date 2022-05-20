f5d282dd8ca0858c79b96245447d8b8f4939bd57
deepthi.vemula <deepthi.vemula@servicenow.com>
2022-05-20 08:14:42 +0530

    week1: git first commit

---
+++ src/main/java/com/scalerpractice/scalerpracticedemo/ScalerPracticeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.scalerpractice.scalerpracticedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerPracticeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerPracticeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scalerpractice/scalerpracticedemo/ScalerPracticeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalerpractice.scalerpracticedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerPracticeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
